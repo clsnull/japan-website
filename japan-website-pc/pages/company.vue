55c00d5bc28314fdf2988c347d33110fe9072b90
clsnull <1334084021@qq.com>
2023-06-09 21:45:38 +0800

    调整

--- japan-website-pc/pages/company.vue
+++ japan-website-pc/pages/company.vue
@@ -54,7 +54,7 @@
                             >
                         </div>
                         <div
-                            v-if="item.type == 3"
+                            v-if="false"
                             class="box-border flex flex-row flex-wrap justify-between"
                         >
                             <div
@@ -63,7 +63,7 @@
                                 :key="i"
                             >
                                 <div class="flex flex-row items-center py-1">
-                                    <label class="w-16 py-1 mr-3 text-center bg-gray-100">社名:</label>
+                                    <label class="w-16 py-1 mr-3 text-center bg-gray-100">拠点:</label>
                                     <span class="flex-1">{{ row.title }}</span>
                                 </div>
                                 <div class="flex flex-row items-center py-1">
@@ -72,12 +72,12 @@
                                 </div>
                                 <div class="flex flex-row items-center py-1">
                                     <label class="w-16 py-1 mr-3 text-center bg-gray-100">TEL:</label>
-                                    <span class="flex-1">{{ row.tel }}</span>
+                                    <span class="flex-1">+ {{ row.tel }}</span>
                                 </div>
-                                <div class="flex flex-row items-center py-1">
+                                <!-- <div class="flex flex-row items-center py-1">
                                     <label class="w-16 py-1 mr-3 text-center bg-gray-100">FAX:</label>
                                     <span class="flex-1">{{ row.fax }}</span>
-                                </div>
+                                </div> -->
                                 <div class="flex flex-row items-center py-1">
                                     <label class="w-16 py-1 mr-3 text-center bg-gray-100">E-mail:</label>
                                     <span class="flex-1">{{ row.email }}</span>
@@ -86,9 +86,51 @@
                         </div>
                     </div>
                 </div>
+                <div class="flex flex-col">
+                    <div
+                        class="flex flex-col justify-center px-5 h-14 w-72 max-sm:bg-gray-200 max-sm:w-full"
+                    >
+                        <label class="block text-lg font-bold">海外拠点</label>
+                    </div>
+                    <div
+                        class="flex flex-col flex-1 py-5 pr-0"
+                    >
+                        <div
+                            class="box-border flex flex-col justify-between flex-1"
+                        >
+                            <div
+                                class="py-3 px-5 mb-[1%] text-base border-t w-full basis-1"
+                                v-for="(row, i) in abc"
+                                :key="i"
+                            >
+                                <div class="flex flex-row items-center py-1">
+                                    <label class="w-20 py-1 mr-3 text-center bg-gray-100 ">拠点:</label>
+                                    <span class="flex-1">{{ row.title }}</span>
+                                </div>
+                                <div class="flex flex-row items-center py-1">
+                                    <label class="w-20 py-1 mr-3 text-center bg-gray-100">所在地:</label>
+                                    <span class="flex-1">{{ row.address }}</span>
+                                </div>
+                                <div class="flex flex-row items-center py-1">
+                                    <label class="w-20 py-1 mr-3 text-center bg-gray-100">TEL:</label>
+                                    <span class="flex-1">+ {{ row.tel }}</span>
+                                </div>
+                                <!-- <div class="flex flex-row items-center py-1">
+                                    <label class="w-20 py-1 mr-3 text-center bg-gray-100">FAX:</label>
+                                    <span class="flex-1">{{ row.fax }}</span>
+                                </div> -->
+                                <div class="flex flex-row items-center py-1">
+                                    <label class="w-20 py-1 mr-3 text-center bg-gray-100">E-mail:</label>
+                                    <span class="flex-1">{{ row.email }}</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <div
-                    class="flex flex-row px-5 pt-3 pb-3 border-t border-gray-100 h-96"
+                    class="flex flex-col px-5 pt-3 pb-3 border-t border-gray-100 h-96"
                 >
+                    <div class="pl-2 mb-2 font-bold border-l-[3px] border-sky-500">本社 〒 550-0013　大阪市西区新町4-21-7</div>
                     <maps></maps>
                 </div>
             </div>
@@ -124,7 +166,7 @@ export default {
                     type: 1,
                 },
                 {
-                    lable: "所在地",
+                    lable: "本社所在地",
                     value: "〒 550-0013　大阪市西区新町4-21-7 2F",
                     type: 1,
                 },
@@ -160,10 +202,59 @@ export default {
                         "工業用品",
                     ],
                 },
-                {
-                    lable: "海外拠点",
-                    type: 3,
-                    value: [
+                // {
+                //     lable: "海外拠点",
+                //     type: 3,
+                //     value: [
+                //         {
+                //             title: "深圳公司",
+                //             address:
+                //                 "广东省深圳市龙华新区梅龙大道906号创业楼3楼",
+                //             tel: "86-755-2857-8111",
+                //             fax: "86-755-2857-8000",
+                //             email: "sz@tamasaki.com",
+                //         },
+                //         {
+                //             title: "北京公司",
+                //             address: "北京市通州区江米店街1号院5号楼16层1612",
+                //             tel: "86-10-6786-8591",
+                //             fax: "86-10-6786-8081",
+                //             email: "bj@tamasaki.com",
+                //         },
+                //         {
+                //             title: "苏州公司",
+                //             address: "苏州高新区竹园路7号2幢1201室",
+                //             tel: "86-512-6708-9211",
+                //             fax: "86-512-6708-8433",
+                //             email: "suz@tamasaki.com",
+                //         },
+                //         {
+                //             title: "重庆公司",
+                //             address: "重庆市沙坪坝区龙湖拉特芳斯1街区3栋1112号",
+                //             tel: "86-23-6637-6663",
+                //             fax: "86-23-6637-6669",
+                //             email: "cq@tamasaki.com",
+                //         },
+                //         {
+                //             title: "成都公司",
+                //             address:
+                //                 "成都市郫都区郫筒镇创智东一路8号银座A栋1801室",
+                //             tel: "86-28-6874-9616",
+                //             fax: "86-28-6874-9615",
+                //             email: "cd@tamasaki.com",
+                //         },
+                //         {
+                //             title: "江西公司",
+                //             address:
+                //                 "江西省萍乡市安源区八一街总部经济大楼3125室",
+                //             tel: "0799-6669801",
+                //             fax: "0799-6669801",
+                //             email: "px@tamasaki.com",
+                //         },
+                //     ],
+                // },
+            ],
+            abc: [
                         {
                             title: "深圳公司",
                             address:
@@ -210,8 +301,6 @@ export default {
                             email: "px@tamasaki.com",
                         },
                     ],
-                },
-            ],
         };
     },
 };
